42e8c2871bb0c9eddd44d7b3b593d7b1c8a23c5c
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-05-19 23:58:45 +1000

    ⚙️setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={classes.wrapper}>
+      
+    </main>
+  );
+}
