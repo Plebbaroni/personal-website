b1cb06f394ec0c16a6f10838d9a8d111d0ef0402
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-05-21 02:52:43 +1000

    🧗‍infocard

--- frontend/src/components/infopanel/Infopanel.tsx
+++ frontend/src/components/infopanel/Infopanel.tsx
@@ -7,7 +7,13 @@ function infopanel() {
       <div className={classes.imgwrapper}>
         <Image src="/icons/meimgfull.jpeg" alt="greenicon" className={classes.meimg} fill style={{ objectFit: 'cover', objectPosition: 'center' }}/>
       </div>
+      <div className={classes.content}>
         <h2 className={classes.greeting}>Hi, I'm{' '}<span className={classes.name}>Jared.</span>{' '}Nice to meet you!</h2>
+        <p className={classes.greetingbody}>I'm a 2nd year CS Student at UNSW. On the right you'll find featured projects as well as the latest posts of my blog(eventually). If you'd like to contact me, please check the links below. Thanks for dropping by! :D</p>
+        <div className={classes.socialsdiv}>
+
+        </div>
+      </div>
     </div>
   )
 }
