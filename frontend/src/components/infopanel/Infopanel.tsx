5b1f430680a6153986cd0af32c018041ca1a0c68
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-05-24 02:16:56 +1000

    🧌appease eslint

--- frontend/src/components/infopanel/Infopanel.tsx
+++ frontend/src/components/infopanel/Infopanel.tsx
@@ -8,8 +8,8 @@ function infopanel() {
         <Image src="/icons/meimgfull.jpeg" alt="greenicon" className={classes.meimg} fill style={{ objectFit: 'cover', objectPosition: 'center' }}/>
       </div>
       <div className={classes.content}>
-        <h2 className={classes.greeting}>Hi, I'm{' '}<span className={classes.name}>Jared.</span>{' '}Nice to meet you!</h2>
-        <p className={classes.greetingbody}>I'm a 2nd year CS Student at UNSW. On the right you'll find featured projects as well as the latest posts of my blog(eventually). If you'd like to contact me, please check the links below. Thanks for dropping by! :D</p>
+        <h2 className={classes.greeting}>I{'\'m'}{' '}<span className={classes.name}>Jared.</span>{' '}Nice to meet you!</h2>
+        <p className={classes.greetingbody}>I{'\'m'}{' '} a 2nd year CS Student at UNSW. On the right you{'\'l'}l find featured projects as well as the latest posts of my blog(eventually). If you{'\'d'} like to contact me, please check the links below. Thanks for dropping by! :D</p>
         <div className={classes.socialsdiv}>
           <Image src="/github.png" alt="greenicon" className={classes.socicon} height={40} width={40}/>
         </div>
