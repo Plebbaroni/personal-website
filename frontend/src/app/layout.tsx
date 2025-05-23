550c237771d9e67fc6530baffd74beb73ce27efe
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-05-24 02:09:29 +1000

    🦇changed to relative import instead of alias

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import './globals.css';
-import Navbar from "@/components/navbar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 export const metadata: Metadata = {
   title: "jaredschulz",
   description: "Personal Website",
